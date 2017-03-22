86d38d2de21304431d8387c2d0c49d0db9b3f530
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-03-22 22:41:10 +0900

    Add column regulation to user

--- chat-space/db/schema.rb
+++ chat-space/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322130822) do
+ActiveRecord::Schema.define(version: 20170322132508) do
 
   create_table "group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "group_id",   null: false
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 20170322130822) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name"
+    t.string   "name",                                null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20170322130822) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["name"], name: "index_users_on_name", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
