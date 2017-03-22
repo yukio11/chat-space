61e8aaf6566bca008d74d2f20b56d6099f509286
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-03-22 22:13:23 +0900

    Add association between group and user

--- chat-space/app/models/user.rb
+++ chat-space/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :messages
+  has_many :groups, through: :group_users
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
