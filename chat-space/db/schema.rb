dc771ff0de1449f7293bd2b232234252125c2adc
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-03-22 20:06:11 +0900

    remaking user model and table

--- chat-space/db/schema.rb
+++ chat-space/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322094034) do
+ActiveRecord::Schema.define(version: 20170322110301) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
