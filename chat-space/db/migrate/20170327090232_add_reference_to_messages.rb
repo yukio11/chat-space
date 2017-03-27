704f64d98ff5e1e254f753a21cac21ae20c7da44
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-03-27 20:39:36 +0900

    add index and not null constraint to messages

--- chat-space/db/migrate/20170327090232_add_reference_to_messages.rb
+++ chat-space/db/migrate/20170327090232_add_reference_to_messages.rb
@@ -1,6 +1,6 @@
 class AddReferenceToMessages < ActiveRecord::Migration[5.0]
   def change
-    add_reference :messages, :group, foreign_key: true
-    add_reference :messages, :user, foreign_key: true
+    add_reference :messages, :group, foreign_key: true, null: false, index: true
+    add_reference :messages, :user, foreign_key: true, null: false, index: true
   end
 end
