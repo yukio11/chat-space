e8d99a80c55b13096d1eb78a1bb086670a7c3296
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-03-27 20:39:57 +0900

    add index and not null constraint to group_users

--- chat-space/db/migrate/20170327095742_add_reference_to_group_users.rb
+++ chat-space/db/migrate/20170327095742_add_reference_to_group_users.rb
@@ -1,6 +1,6 @@
 class AddReferenceToGroupUsers < ActiveRecord::Migration[5.0]
   def change
-    add_reference :group_users, :group, foreign_key: true
-    add_reference :group_users, :user, foreign_key: true
+    add_reference :group_users, :group, foreign_key: true, null: false, index: true
+    add_reference :group_users, :user, foreign_key: true, null: false, index: true
   end
 end
