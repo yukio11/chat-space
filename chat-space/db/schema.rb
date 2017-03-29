f8ff90d7bccdbb17ea8058b2944947f68b4f0c5d
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-03-29 21:36:22 +0900

    remove index to group name

--- chat-space/db/schema.rb
+++ chat-space/db/schema.rb
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 20170322120539) do
     t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_groups_on_name", using: :btree
   end
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
