e34c1d0ae7f7ca58c391f78968d1f976a07676a9
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-04-01 19:20:11 +0900

    Add flash messages in Japanese

--- chat-space/config/application.rb
+++ chat-space/config/application.rb
@@ -2,6 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -13,5 +14,7 @@ module ChatSpace
       g.test       false
       g.helper     false
     end
+
+    config.i18n.default_locale = :ja #日本語化
   end
 end
