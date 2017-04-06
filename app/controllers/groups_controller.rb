c5a995ad0a11819bb756e8c1a91113d27f336d43
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-04-06 21:10:23 +0900

    Make the create action in groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,5 +4,12 @@ class GroupsController < ApplicationController
   end
 
   def create
+    @group = Group.create(group_params)
+    redirect_to controller: :chat, action: :index
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name,{ user_ids: []})
   end
 end
