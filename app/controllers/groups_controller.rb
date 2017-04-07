64f2fcb4ecd39c69f91275a8467a4ba9efd18f3f
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-04-07 19:01:30 +0900

    Add flash messages in creating and updating a group
    Make a show page for group
    Add a flash message in creating a group
    Add a flash message in updating a group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,15 +1,44 @@
 class GroupsController < ApplicationController
+  def show
+    @group = Group.find(params[:id])
+  end
   def new
     @group = Group.new
   end
 
   def create
     @group = Group.create(group_params)
-    redirect_to controller: :chat, action: :index
+    if @group.save
+      redirect_to group_path(@group), notice: "グループ作成が完了しました。"
+    else
+      flash[:alert] = "グループの作成に失敗しました。"
+      render :new
+    end
   end
+
+  def edit
+    set_group
+  end
+
+  def update
+    set_group
+    @group.update(group_params)
+    if @group.save
+      redirect_to group_path(@group), notice:"グループの更新が完了しました。"
+    else
+      flash[:alert] = "グループの更新に失敗しました。"
+      render :edit
+    end
+  end
+
 
   private
   def group_params
     params.require(:group).permit(:name,{ user_ids: []})
   end
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
 end
