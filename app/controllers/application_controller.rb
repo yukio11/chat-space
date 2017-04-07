8aa993fb0a9d8918ffe4e77c4bd8dd6b395f19de
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-04-07 19:33:30 +0900

    Fix redirecting
    When a user signout, app redirect him to signin page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  def after_sign_out_path_for(resource)
-    new_user_session_path
-  end
   protected
 
   def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :groups
   resources :chat, only: [:index]
+  root "chat#index"
 end
