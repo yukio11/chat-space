6a8f867e0ea54cfeff5cf1957d7a4c905be9e592
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-04-08 12:17:08 +0900

    Add new and create action
    To send a message in a group

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,9 +6,12 @@ class MessagesController < ApplicationController
   end
 
   def new
+    @group = Group.find(params[:group_id])
+    @message = Message.new
   end
 
   def create
+    @group = Message.create(message_params)
   end
 
   def edit
@@ -19,4 +22,10 @@ class MessagesController < ApplicationController
 
   def delete
   end
+
+  private
+  def message_params
+    params.require(:message).permit(:body, :image, :group_id, :user_id).merge(group_id: params[:group_id], user_id: current_user.id)
+  end
+
 end
