53451a4fab70123919c9dcbc51f9802116eb206d
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-04-12 18:26:48 +0900

    Fix the show function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,5 @@
 class MessagesController < ApplicationController
 
-  before_action :set_message, only: [:edit, :update, :destroy]
-  def index
-    @messages = Message.where(group_id: params[:group_id])
-  end
-
   def new
     @group = Group.find(params[:group_id])
     @message = Message.new
@@ -12,27 +7,11 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.create(message_params)
-    redirect_to group_messages_path
-  end
-
-
-  def update
-    @message = @message.update(message_params)
-    redirect_to group_messages_path
-  end
-
-  def destroy
-    @message = @message.destroy
-    redirect_to group_messages_path
+    redirect_to group_path(params[:group_id])
   end
 
   private
   def message_params
-    params.require(:message).permit(:body, :image, :group_id, :user_id).merge(group_id: params[:group_id], user_id: current_user.id)
-  end
-
-  def set_message
-    @message = Message.find(params[:id])
-    @group = Group.find(params[:group_id])
+    params.require(:message).permit(:body ,:image ,:group_id , :user_id).merge(group_id: params[:group_id], user_id: current_user.id)
   end
 end
