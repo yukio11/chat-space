e57d2880ae4f325a1c99091e29593acef7059f89
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-04-12 17:36:01 +0900

    Remove the not null constraint to the image column in the message table

--- db/migrate/20170322110310_create_messages.rb
+++ db/migrate/20170322110310_create_messages.rb
@@ -2,7 +2,7 @@ class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
       t.text :body
-      t.string :image, null: false
+      t.string :image
       t.references :group, foreign_key: true, null: false, index: true
       t.references :user, foreign_key: true, null: false, index: true
       t.timestamps null: false
