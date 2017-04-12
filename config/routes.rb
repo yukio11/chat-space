9376bf25dc3535fadaf9ff3126a963436670cca1
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-04-13 00:33:03 +0900

    Fix routing
    Write a code simply

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :groups, only: [:index, :new, :edit] do
+  resources :groups, except: [:show, :update, :destroy] do
     resources :messages, only:[:index, :create]
   end
   root "groups#index"
