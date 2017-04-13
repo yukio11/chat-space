306715a3ae36f92eee989706169456fc31e9f7ee
Yukihiro <yukihiro.ueda.1105@gmail.com>
2017-04-13 15:06:22 +0900

    Fix a conditional branch
    Write the process after the validation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,10 +11,12 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.create(message_params)
-    unless @message.valid?
+    if @message.valid?
+      redirect_to group_messages_path(params[:group_id])
+    else
       flash[:notice] = "メッセージを入力してください"
+      redirect_to group_messages_path(params[:group_id])
     end
-    redirect_to group_messages_path(params[:group_id])
   end
 
   private
